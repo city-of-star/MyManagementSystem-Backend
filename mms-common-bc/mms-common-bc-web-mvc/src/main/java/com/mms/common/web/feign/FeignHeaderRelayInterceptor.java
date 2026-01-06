b2564e2de125047f5a59f9d08790cd30daaa2333
li.hongyu <li.hongyu@ustcinfo.com>
2026-01-06 14:05:42 +0800

    优化Feign调用过滤器日志打印

--- mms-common-bc/mms-common-bc-web-mvc/src/main/java/com/mms/common/web/feign/FeignHeaderRelayInterceptor.java
+++ mms-common-bc/mms-common-bc-web-mvc/src/main/java/com/mms/common/web/feign/FeignHeaderRelayInterceptor.java
@@ -48,17 +48,21 @@ public class FeignHeaderRelayInterceptor implements RequestInterceptor {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         if (attributes == null) {
             // 不在 HTTP 请求上下文中（例如定时任务触发的 Feign 调用），跳过
-            log.debug("Feign 透传拦截器：当前不在 HTTP 请求上下文中，跳过网关请求头透传");
+            log.info("Feign请求头透传：不在HTTP请求上下文，跳过 | 目标: {} {}", 
+                    template.method(), template.url());
             return;
         }
 
         HttpServletRequest request = attributes.getRequest();
+        String targetUrl = template.method() + " " + template.url();
+        
+        // 统计透传的头信息
+        String traceId = null;
 
         // 透传 Authorization 头
         String authHeader = request.getHeader(JwtConstants.Headers.AUTHORIZATION);
         if (StringUtils.hasText(authHeader)) {
             template.header(JwtConstants.Headers.AUTHORIZATION, authHeader);
-            log.debug("Feign 透传拦截器：透传 Authorization 头");
         }
 
         // 透传网关相关请求头
@@ -66,9 +70,20 @@ public class FeignHeaderRelayInterceptor implements RequestInterceptor {
             String value = request.getHeader(headerName);
             if (StringUtils.hasText(value)) {
                 template.header(headerName, value);
-                log.debug("Feign 透传拦截器：透传请求头 {} = {}", headerName, value);
+                
+                // 记录关键头信息用于排查
+                if (GatewayConstants.Headers.TRACE_ID.equals(headerName)) {
+                    traceId = value;
+                }
             }
         }
+
+        // 记录透传摘要信息
+        StringBuilder logMsg = new StringBuilder("Feign请求头透传 | 目标: ").append(targetUrl);
+        if (traceId != null) {
+            logMsg.append(" | TraceId: ").append(traceId);
+        }
+        log.info(logMsg.toString());
     }
 }
 
