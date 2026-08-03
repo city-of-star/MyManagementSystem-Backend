f1d10e827c8851be74773d2762c43629509d6ab5
li.hongyu <2722562862@qq.com>
2026-08-03 20:05:36 +0800

    收窄 mms-common-bc-core 的依赖

--- mms-gateway-bc/src/main/java/com/mms/gateway/GatewayApplication.java
+++ mms-gateway-bc/src/main/java/com/mms/gateway/GatewayApplication.java
@@ -3,20 +3,8 @@ package com.mms.gateway;
 import com.mms.common.core.constants.scan.PackageScanConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
 
-/**
- * 网关不连库。但依赖了 mms-common-bc-core（内含 mybatis-plus-starter → JDBC），
- * 若不排除会触发 DataSource 自动配置，启动失败：Failed to determine a suitable driver class。
- */
-@SpringBootApplication(
-        scanBasePackages = PackageScanConstants.GATEWAY_PACKAGE_SCAN,
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                DataSourceHealthContributorAutoConfiguration.class
-        }
-)
+@SpringBootApplication(scanBasePackages = PackageScanConstants.GATEWAY_PACKAGE_SCAN)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(com.mms.gateway.GatewayApplication.class, args);
